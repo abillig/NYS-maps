2a1f557d0f7f3f470bdd47283272bb8ee0ac5f48
Avram Billig <avrambillig@gmail.com>
2016-11-18 23:23:00 -0500

    mapping start

---
+++ alter_json.js
@@ -0,0 +1,6 @@
+fs = require('fs');
+// var m = JSON.parse(fs.readFileSync('people.json').toString());
+// m.forEach(function(p){
+//     p.pic = p.name.toLowerCase()+".png";
+// });
+// fs.writeFile('people.json', JSON.stringify(m));
